b973e73cbab0d58890c665f2820fdbc58c34506d
Yash Shah <yashshah1282@gmail.com>
2024-11-25 11:31:06 +0300

    Security measures

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { Navigation } from "@/components/Navigation"
 import { Footer } from "@/components/Footer"
+import { PasswordProtection } from "@/components/PasswordProtection"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navigation />
-        {children}
-        <Footer />
+        <PasswordProtection>
+          <Navigation />
+          {children}
+          <Footer />
+        </PasswordProtection>
       </body>
     </html>
   )

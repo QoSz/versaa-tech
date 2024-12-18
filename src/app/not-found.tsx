2f30c029c1d9773ad68f652645b604a225f89eec
Yash Shah <yashshah1282@gmail.com>
2024-12-18 10:19:17 +0300

    Fixing compile errors

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -20,7 +20,7 @@ export default function NotFound() {
             Page Not Found
           </h2>
           <p className="text-gray-600 max-w-md mx-auto">
-            We couldn't find the page you're looking for. It might have been moved or doesn't exist.
+            We couldn&apos;t find the page you&apos;re looking for. It might have been moved or doesn&apos;t exist.
           </p>
         </div>
 

f662b97e8a937696a6a08b22cc52a886de2d7f36
Yash Shah <yashshah1282@gmail.com>
2025-01-27 18:19:06 +0300

    Added back contact to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,26 +5,34 @@ import { Overview } from "@/components/home/Overview";
 import { Industries } from "@/components/home/Industries";
 import { Differentiators } from "@/components/home/Differentiators";
 import { Regions } from "@/components/home/Regions";
+import ContactPage from "./contact/page";
+
+// Define the section spacing class in a variable. This is used to off set the navbar covering the 
+// titles of the sections.
+const sectionSpacing = "pt-[90px] -mt-[90px]";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <main className="bg-[#f7fffc]">
         <Hero />
-        <section id="overview">
+        <section id="overview" className={sectionSpacing}>
           <Overview />
         </section>
-        <section id="industries">
+        <section id="industries" className={sectionSpacing}>
           <Industries />
         </section>
-        <section id="differentiators">
+        <section id="differentiators" className={sectionSpacing}>
           <Differentiators />
         </section>
         <Regions />
-        <section id="about">
+        <section id="about" className={sectionSpacing}>
           <About />
-          <section id="about-leadership">
+          <section id="about-leadership" className={sectionSpacing}>
             <Team />
+          </section>
+          <section id="contact">
+            <ContactPage />
           </section>
         </section>
       </main>
